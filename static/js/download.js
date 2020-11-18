13aa7610baf5a6f301b78381b8411fe7e2cf8f2f
untone <anton@vvvv.org>
2020-11-18 16:12:09 +0100

    Prepare to test online

--- static/js/download.js
+++ static/js/download.js
@@ -5,17 +5,24 @@ function getLatestBuild()
     let proxy = "https://api.codetabs.com/v1/proxy?quest="
     let teamcity = "https://teamcity.vvvv.org"
     let builds = "/guestAuth/app/rest/builds/buildType:vvvv_gamma_stride_Build,branch:default:any,status:success/artifacts/children"
-    let VersionPattern = /vvvv_gamma_(\d{4}\.\d)/;
+    let versionPattern = /vvvv_gamma_(\d{4}\.\d)/;
 
     fetch(proxy+teamcity+builds)
         .then(response => response.text())
         .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
         .then(data => {
-            var href=data.querySelector('file content[href*="exe"]').getAttribute('href');
-            var version=href.match(VersionPattern)[1]
+            var file = data.querySelector('file content[href$=".exe"]');
+            
+            if (file != null)
+            {
+                var href = file.getAttribute('href');
+                var version=href.match(versionPattern)[1];
 
-            document.getElementById('previewButton').href = teamcity+href;
-            document.getElementById('previewVersion').innerHTML += " " + version;
-            document.getElementById('gammaDownloads').style = "visibility: visible";
+                console.log (href);
+                console.log (version);
+                //document.getElementById('previewButton').href = teamcity+href;
+                //document.getElementById('previewVersion').innerHTML += " " + version;
+                //document.getElementById('gammaDownloads').style = "visibility: visible";
+            }   
         })
 }
