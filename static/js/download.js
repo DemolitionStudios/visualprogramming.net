909c81bd8f92991ee71897b0a1cc3dc2246212f0
untone <anton@vvvv.org>
2020-11-20 00:29:16 +0100

    Added 3 Preview buttons. WIP

--- static/js/download.js
+++ static/js/download.js
@@ -1,28 +1,49 @@
-getLatestBuild();
+//getLatestBuild();
 
 function getLatestBuild()
 {
-    let proxy = "https://api.codetabs.com/v1/proxy?quest="
-    let teamcity = "https://teamcity.vvvv.org"
-    let builds = "/guestAuth/app/rest/builds/buildType:vvvv_gamma_stride_Build,branch:default:any,status:success/artifacts/children"
-    let versionPattern = /vvvv_gamma_(\d{4}\.\d)/;
+    let teamcity = "https://teamcity.vvvv.org";
+    let proxy = "https://api.codetabs.com/v1/proxy?quest=";
+    let builds = "/guestAuth/app/rest/builds/?status=success&buildType=vvvv_gamma_stride_Build&state=finished&count=3"
+    let versionPattern = /(.*?)\+/;
+    let link = proxy+teamcity+builds;
+    //let link = teamcity+builds
 
-    fetch(proxy+teamcity+builds)
+    fetch(link)
         .then(response => response.text())
         .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
         .then(data => {
-            var file = data.querySelector('file content[href$=".exe"]');
+            var builds = data.getElementsByTagName("build");
             
-            if (file != null)
-            {
-                var href = file.getAttribute('href');
-                var version=href.match(versionPattern)[1];
 
-                console.log (href);
-                console.log (version);
-                //document.getElementById('previewButton').href = teamcity+href;
-                //document.getElementById('previewVersion').innerHTML += " " + version;
-                //document.getElementById('gammaDownloads').style = "visibility: visible";
-            }   
+            for (let build of builds) {
+                let buildNumber = build.getAttribute("number");
+                let href = build.getAttribute ("href");
+                let link = "";
+
+                if (href != null)
+                {
+                    fetch(proxy + teamcity + href + "/artifacts/children")
+                    .then(response => response.text())
+                    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+                    .then(data => {
+                        var file = data.querySelector('file content[href$=".exe"]');
+                        
+                        if (file != null)
+                        {
+                            link = file.getAttribute('href');
+                        }   
+                    })
+                }
+
+                if (link != null)
+                {
+                   let shortNumber = buildNumber.match(versionPattern)[1];
+                   document.getElementById('gammaDownloads').innerHTML += 
+                   `<a href="${teamcity+link}" class="btn btn-secondary previewButton">Preview ${shortNumber}</a>`;  
+                }
+            }
+
+            document.getElementById('gammaDownloads').style = "visibility: visible"; 
         })
 }
