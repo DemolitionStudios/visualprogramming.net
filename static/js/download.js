610792a270254e402a5ea8b5d10c1ed346a1e315
untone <anton@vvvv.org>
2020-11-22 22:59:27 +0100

    Added "changes" links

--- static/js/download.js
+++ static/js/download.js
@@ -21,7 +21,8 @@ async function getLatestBuild()
         for (var preview of previews)
         {
             document.getElementById('previews').innerHTML += 
-            `<a href="${teamcity}${preview.link}" class="btn btn-secondary previewButton">Preview ${preview.buildNumber}</a><br/>`;
+            `<a href="${teamcity}${preview.link}" class="btn btn-secondary previewButton">Preview ${preview.buildNumber}</a>`+
+            `&nbsp; &nbsp; <a href="${preview.changesLink}" target="_blank">Changes</a><br/>`;
         }
 
         document.getElementById('loading').remove();   
@@ -43,6 +44,7 @@ async function fetchData(link)
     for (let build of builds) {
         
         let buildNumber = build.getAttribute("number");
+        let id = build.getAttribute ("id");
         let href = build.getAttribute ("href");
 
         if (href != null)
@@ -60,7 +62,8 @@ async function fetchData(link)
                     if (exeLink != null)
                     {
                         let shortNumber = buildNumber.match(versionPattern)[1];
-                        previews.push ({link: exeLink, buildNumber: shortNumber});
+                        let changes = `https://teamcity.vvvv.org/viewLog.html?buildId=${id}&tab=buildChangesDiv`;
+                        previews.push ({link: exeLink, buildNumber: shortNumber, changesLink: changes});
                     }
                 }   
             })
